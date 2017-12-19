465e847349e84123ba86d53fe5bdb053bd09a8e1
SakiiR SakiiR <sakiirlessons@gmail.com>
2017-12-19 11:16:02 +0100

    Facto :fireworks:

--- fuzzy/fuzzy.py
+++ fuzzy/fuzzy.py
@@ -70,12 +70,20 @@ class Fuzzy(object):
         url = self._url.replace(self._tag, word)
         return Request(url=url, headers=new_headers, data=new_data, verb=self._verb, word=word, timeout=self._timeout)
 
-    async def response_content(self, response):
+    def status(self, spent, word):
 
-        """ Read all the content from the HTTP response """
+        """ Display the progress status to the console """
 
-        return await response.text()
-
+        self._requests_did += 1
+        self._percent_done = (self._requests_did * 100) / self._requests_todo
+        started_duration = datetime.now() - self._starttime
+        self._p.status('{}/{} {}% {}req/s - {}'.format(
+            self._requests_did,
+            self._requests_todo,
+            int(self._percent_done),
+            int(self._requests_did / (started_duration.seconds + 1)),
+            word,
+        ))
 
     async def call_request(self, request):
 
@@ -86,17 +94,8 @@ class Fuzzy(object):
         spent = int((time.time() - self._last_start) * 1000)
         response = data['response']
         content = data['text']
-        self._requests_did += 1
-        self._percent_done = (self._requests_did * 100) / self._requests_todo
-        started_duration = datetime.now() - self._starttime
         if not self._disable_progress:
-            self._p.status('{}/{} {}% {}req/s - {}'.format(
-                self._requests_did,
-                self._requests_todo,
-                int(self._percent_done),
-                int(self._requests_did / (started_duration.seconds + 1)),
-                request._word,
-            ))
+            self.status(spent, request._word)
         if Matching.is_matching(response.status, content, hc=self._hc, sc=self._sc, ht=self._ht, st=self._st):
             color = Printer.get_code_color(response.status)
             Printer.one("'" + request._word + "'", str(response.status), str(spent), str(len(content)), color, str(len(content.split(' '))), str(len(content.splitlines())), str(request._word in content))
@@ -122,6 +121,14 @@ class Fuzzy(object):
             await self._queue.put(request)
         self._requests_todo = self._queue.qsize()
 
+    async def trigger_coros(self):
+
+        """ Launch and gather the coroutines """
+
+        coros = (self.consumer() for _ in range(self._limit))
+        task = self.loop.create_task(asyncio.gather(*coros))
+        await self._queue.join()
+
     async def process(self):
 
         """ Main task to process
@@ -132,18 +139,13 @@ class Fuzzy(object):
         """
 
         self._queue = asyncio.Queue()
-        log.success("Url Available ! ({})".format(self._url))
-        log.info("Filling tasks queue !")
         await self.fill_queue()
-        log.success("Tasks queue ready !")
         log.info("Launching {} requests ..".format(self._requests_todo))
         if not self._disable_progress:
             self._p = log.progress('Status')
         Printer.first()
         self._starttime  = datetime.now()
-        coros = (self.consumer() for _ in range(self._limit))
-        task = self.loop.create_task(asyncio.gather(*coros))
-        await self._queue.join()
+        await self.trigger_coros()
 
     def loop(self):
 
